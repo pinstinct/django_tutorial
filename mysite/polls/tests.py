fefe396f0c69338eb3f008b819e33c69d28671f2
LimHM <gusals3407@gmail.com>
2017-02-05 20:26:50 +0900

    was_published_recently() 메서드를 검사하는 테스트 작성

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        pub_date 값이 미래의 날짜라면,
+        was_published_recently()는 False를 반환해야 합니다.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
