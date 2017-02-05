6b4452e5f7830b9cdd9ae80fcba40bbd94abb5d2
LimHM <gusals3407@gmail.com>
2017-02-05 20:05:12 +0900

    주석 제거

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,16 +3,17 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
-        #return 'question_text: {}, pub_date: {}'.format(self.question_text, self.pub_date)
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,4 +22,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-        #return 'qestion: {}, choice_text: {}, votes: {}'.format(self.question, self.choice_text, self.votes)
+    
