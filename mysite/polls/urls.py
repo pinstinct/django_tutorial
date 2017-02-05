3585108517fdcef90e2ee7f3518861aeb34df9dc
LimHM <gusals3407@gmail.com>
2017-02-05 14:24:30 +0900

    app_name 추가

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 # 작성한 views.py 파일을 import
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
         # /polls/
         url(r'^$', views.index, name='index'),
